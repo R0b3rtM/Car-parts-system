a687a8aa9bb348d8a90ee4774a5c48907b4df410
R0b3rtM <168562199+R0b3rtM@users.noreply.github.com>
2025-10-01 13:51:38 +0300

    Version 0.1
    Added:
    Json files - database file and config file
    UI - Get inputs from user to add parts to database file

---
+++ Class_and_Inheritance/src/Main_pkg/Extractor.java
@@ -0,0 +1,19 @@
+package Main_pkg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Extractor {
+	
+	private List<Part> parts;
+	
+	public Extractor() {
+		this.parts = new ArrayList<>();
+	}
+	
+	public void upload_parts() {
+		//Upload parts in to parts list from the Json data file
+	}
+}
